a3f422f7c2d9d5f9c4049e4fa9c665104cc9a50d
Qi Wei <75564742+WEIQI2514@users.noreply.github.com>
2022-11-20 17:45:47 -0500

    Update as3.py

--- as3.py
+++ as3.py
@@ -1,56 +1,22 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "c751a675",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import pandas as pd\n",
-    "import streamlit as st\n",
-    "import matplotlib.pylot as plt\n",
-    "import seaborn as sns\n",
-    "import numpy as np\n",
-    "import json\n",
-    "\n",
-    "st.title(\"Group_2_assignment3\")\n",
-    "\n",
-    "#Fetch tweets related to \"Haribo\" using a command:\n",
-    "#snscrape --jsonl --progress --max-results 500 --since 2022-10-01 twitter-seach \"#haribo until:2022-10-31\" >haribo.json\n",
-    "\n",
-    "def load_data():\n",
-    "    #Read one twitter per line from haribo.json\n",
-    "    with open(\"haribo.json\",\"r\") as f:\n",
-    "        tweets = f.readlines()\n",
-    "        # Covert json string to dictionary\n",
-    "        tweets = [json.loads(tweet) for tweet in tweets]\n",
-    "        return tweets\n",
-    "    \n",
-    "tweets = load_data()\n",
-    "# Map json keys to dictionary keys\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.13"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import pandas as pd
+import streamlit as st
+import matplotlib.pylot as plt
+import seaborn as sns
+import numpy as np
+import json
+
+st.title("Group_2_assignment3")
+
+#Fetch tweets related to "Haribo" using a command:
+#snscrape --jsonl --progress --max-results 500 --since 2022-10-01 twitter-seach "#haribo until:2022-10-31" >haribo.json
+
+def load_data():
+    #Read one twitter per line from haribo.json
+    with open("haribo.json","r") as f:
+        tweets = f.readlines()
+        # Covert json string to dictionary
+        tweets = [json.loads(tweet) for tweet in tweets]
+        return tweets
+    
+tweets = load_data()
+# Map json keys to dictionary keys
